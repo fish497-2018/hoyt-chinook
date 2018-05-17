4c058ac1fe62f26ea0c06fd1e100b4bef75699e3
sr320 <roberts.sbr@gmail.com>
2018-05-17 10:13:52 -0700

    new graph and dplyr

--- Scripts/chinook_script.R
+++ Scripts/chinook_script.R
@@ -7,10 +7,23 @@ View(chinook_data)
 
 #Create one bar graph for each brood year, where each histogram has one column per averaged density (low,medium,high)
 library(tidyverse)
+
+
+View(chinook_data)
+
 ggplot(chinook_data) + 
   geom_bar(aes(x=Density, y=Percent.Mortality, color=Density)) +    
   facet_wrap(~Broodyear)
 
 
+#this should work
+ggplot(chinook_data, aes(Density, Percent.Mortality)) +
+geom_bar(stat = "identity") +
+  facet_wrap(~Broodyear)
 
 
+
+
+#code to group by density
+group_by(chinook_data, Density) %>%
+  summarize(Mortality = mean(Percent.Mortality)) 
