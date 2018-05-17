3579ebf7fc9cfd3335c3d0cecf53141dfc34b4de
Tyler Hoyt <watermaster@D-10-19-251-76.dhcp4.washington.edu>
2018-05-17 09:32:45 -0700

    Problem with code.
    Error: stat_count() must not be used with a y aesthetic.

--- Scripts/chinook_script.R
+++ Scripts/chinook_script.R
@@ -1,3 +1,16 @@
 library(dplyr)
 chinook_data <- read.csv("Data/OnHatcheryMortalityFigure.csv")
 View(chinook_data)
+
+
+
+
+#Create one bar graph for each brood year, where each histogram has one column per averaged density (low,medium,high)
+library(tidyverse)
+ggplot(chinook_data) + 
+  geom_bar(aes(x=Density, y=Percent.Mortality, color=Density)) +    
+  facet_wrap(~Broodyear)
+
+
+
+
