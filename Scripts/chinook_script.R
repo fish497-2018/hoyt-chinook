ec07e4e7560d6903ea9227c5f12a3fac44cae4d2
sam <samwhite@uw.edu>
2018-05-17 11:32:33 -0700

    add boxplot/dot plot overlay

--- Scripts/chinook_script.R
+++ Scripts/chinook_script.R
@@ -23,3 +23,21 @@ geom_bar(stat = "summary", fun.y = "mean") +
 #code to group by density
 group_by(chinook_data, Density) %>%
   summarize(Mortality = mean(Percent.Mortality)) 
+
+
+# Convert Density column to a factor - using the factor() function.
+# Since factors are ordered alphabetically (e.g. High, Low, Med in this data), we need to reorder
+# by explicitly listing them in alpahabetical order.
+chinook_data$Density <- factor(chinook_data$Density, levels = c('Low', 'Med', 'High'))
+
+# Create box plot of data with overlay of individual data points.
+
+ggplot(data = chinook_data, 
+       aes(x = as.character(Broodyear), y = Percent.Mortality)) + # Converts Broodyear values to character (instead of integer)
+  geom_boxplot(alpha = 0) + # Create box plot & set box plot box transparency to 100% transparent.
+  geom_jitter(alpha = 0.5, color = "blue") + # Create dot plot & set data point transparency to 50% transparent.
+  facet_wrap(~ Density) + # Create mutl-faceted plot, grouped by Density.
+  labs(title = "Percent Mortality per Year
+       by Density",
+       x = "Broodyear",
+       y = "Percent Mortality") # Set labels for title and axes.
